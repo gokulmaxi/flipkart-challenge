63c294f2733dd537bc1cdd414eb4389c63ae3f66
boopesh-mc <boopesh.mc20@bitsathy.ac.in>
2021-09-16 15:08:00 +0530

    on launch

--- src/flipkart_hw/src/moveToGoal.py
+++ src/flipkart_hw/src/moveToGoal.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 from inspect import trace
 import string
 import rospy
-from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
 import paho.mqtt.client as paho
 import json
 from flipBot import flipbot
@@ -12,17 +10,17 @@ def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
 
 
-def servo_control(data: int, topic: string):
+def servo_control(data , topic):
     jsonData = {"data": [0, 0, data, 0]}
     jsonEncoded = json.dumps(jsonData)
-    (rc, mid) = client.publish("test/message", jsonEncoded, qos=1)
+    (rc, mid) = client.publish(topic, jsonEncoded, qos=1)
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", jsonEncoded)
 
 
-def rotate(data: int, topic: string):
+def rotate(data, topic):
     jsonData = {"data": [0, 0, 0, data]}
     jsonEncoded = json.dumps(jsonData)
-    (rc, mid) = client.publish("test/message", jsonEncoded, qos=1)
+    (rc, mid) = client.publish(topic, jsonEncoded, qos=1)
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", jsonEncoded)
 
 
@@ -36,10 +34,10 @@ if __name__ == '__main__':
         x = flipbot.FlipBot(1)
         y = flipbot.FlipBot(2)
         
-        x.move_y(0.46,-0.75)
-        x.rotate()
-        x.move_x(1.5,-0.75)
-        y.move_y()
-        # servo_control(1,"test/message")
+        # x.move_y(0.46,-0.75)
+        # x.rotate()
+        # x.move_x(1.5,-0.75)
+        # y.move_y()
+        servo_control(1,"flipkart/bot1")
     except rospy.ROSInterruptException:
         pass
