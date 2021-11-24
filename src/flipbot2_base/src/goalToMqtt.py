8ed241eaa28279e8e0ef5a29cc77e7c2209e4d5f
gokul <gokulakams@gmail.com>
2021-11-24 11:41:32 +0530

    individual goal to mqtt nodes for each bot

--- src/flipbot2_base/src/goalToMqtt.py
+++ src/flipbot2_base/src/goalToMqtt.py
@@ -1,36 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import rospy
 from geometry_msgs.msg import Twist
 import paho.mqtt.client as paho
 import json
-
+import sys
 
 def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
-
-
+    # linear velocity l_x and l_y
+    # angular velocity a_z
+def omniwheel_velocity(l_x,l_y,a_z):
+    wheel_radius = 0.28
+    w_1 = (l_y/wheel_radius) + a_z
+    w_2 = (l_x/wheel_radius) + a_z
+    w_3 = (l_y/wheel_radius) + a_z
+    w_4 = (l_x/wheel_radius) + a_z
+    return [w_1,w_2,w_3,w_4]
 def callback(data,args):
     bot_index = args
-    jsonData = {"data": [data.linear.x, data.angular.z]}
+    velocity_list = omniwheel_velocity(data.linear.x,data.linear.y,data.angular.z)
+    jsonData = {"data": velocity_list}
     jsonEncoded = json.dumps(jsonData)
     (rc, mid) = client.publish("flipkart/bot"+str(bot_index), jsonEncoded, qos=1)
     rospy.loginfo(rospy.get_caller_id() + "I heard %s and published to %d", jsonEncoded,bot_index)
 
 
-def listener():
+def listener(bot_no):
 
     rospy.init_node('listener_mqtt')
-    rospy.Subscriber("/flipbot1/cmd_vel", Twist, callback,1)
-    rospy.Subscriber("/flipbot2/cmd_vel", Twist, callback,2)
-    rospy.Subscriber("/flipbot3/cmd_vel", Twist, callback,3)
-    rospy.Subscriber("/flipbot4/cmd_vel", Twist, callback,4)
+    rospy.Subscriber("/flipbot1/cmd_vel", Twist, callback,bot_no)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 
 client = paho.Client()
 client.on_publish = on_publish
-client.connect("10.10.174.148", 1883)
+client.connect("10.10.165.7", 1883)
 client.loop_start()
 if __name__ == '__main__':
-    listener()
+    print("\nName of the bot = flibot", sys.argv[1])
+    listener(int(sys.argv[1]))
