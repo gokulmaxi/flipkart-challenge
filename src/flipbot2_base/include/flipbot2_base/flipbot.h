6f9fbfd1d8e068ad8a5fb8e1918b4538942a0c04
gokul <gokulakams@gmail.com>
2021-12-12 19:52:49 +0530

    added linear tolerance dyanamic param

--- src/flipbot2_base/include/flipbot2_base/flipbot.h
+++ src/flipbot2_base/include/flipbot2_base/flipbot.h
@@ -1,51 +1,85 @@
 #include "geometry_msgs/TransformStamped.h"
-#include "math.h" 
-#include <cmath>
-#include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "math.h"
+#include "ros/ros.h"
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
+#include <cmath>
 enum Axis { x, y };
 class Goal {
 public:
   Axis axis;
   float point;
+  Goal() {
+    axis = x;
+    point = 0;
+  }
   Goal(Axis _axis, float _point) {
     axis = _axis;
     point = _point;
   }
 };
-/**
- * @brief: calculate the euclidean distance
- *
- * @param: int startX
- *       : int endX
- *
- * @return: double
- */
-double euclidianDistance(double start, double end) {
-  double distance = 0.0;
-  distance = std::sqrt(pow((start - end), 2));
-  return distance;
-}
+class VelocityController : public Goal {
+private:
+  double *linearPConstantPtr;
+  double *linearTolerance;
+  geometry_msgs::TransformStamped *transformPtr;
+  Goal *goal;
 
-geometry_msgs::Twist calculateVelocity(Goal _goal,double pConstant){
-        geometry_msgs::Twist _twist;
-        if(_goal.axis == x ){
-                ROS_INFO("Moving in X direction");
-                double _linearVel = euclidianDistance(0,_goal.point);
-                _twist.linear.x = 0;
-        }
-        if(_goal.axis == y ){
-                ROS_INFO("Moving in Y direction");
-                double _linearVel = euclidianDistance(0,_goal.point);
-                _twist.linear.y = _linearVel * pConstant;
-        }
+public:
+  VelocityController(double *_linearP, double *_linearTolerance,
+                     geometry_msgs::TransformStamped *_transformPtr) {
+    linearPConstantPtr = _linearP;
+    transformPtr = _transformPtr;
+    linearTolerance = _linearTolerance;
+  }
+  void setGoal(Goal _goal) { *this->goal = _goal; }
+  /**
+   * @brief: calculate the euclidean distance
+   *
+   * @param: int startX
+   *       : int endX
+   *
+   * @return: double
+   */
+  double euclidianDistance() {
+    double _distance = 0.0;
+    /* distance = std::sqrt(pow((start - end), 2)); */
+    if (goal->axis == x)
+      _distance = goal->point - transformPtr->transform.translation.x;
+    if (goal->axis == y)
+      _distance = goal->point - transformPtr->transform.translation.y;
+    return _distance;
+  }
+  /**
+   * @brief: checks if robot is in tolerance
+   *
+   * @param: Goal _goal
+   *
+   * @return: bool
+   */
+  bool inTolerance() {
+    if (abs(euclidianDistance()) < *linearTolerance) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  geometry_msgs::Twist calculateVelocity() {
+    geometry_msgs::Twist _twist;
+    double tolerance =3;
+    if (goal->axis == x) {
+      double _linearVel = euclidianDistance();
+      _twist.linear.x = _linearVel * (tolerance);
+    }
+    if (goal->axis == y) {
+      double _linearVel = euclidianDistance();
+      _twist.linear.y = _linearVel * (tolerance);
+    }
 
-        return _twist;
-}
-
-
+    return _twist;
+  }
+};
 /**
  * @brief updates the transform of the robot.Intentionally created to run as
  * thread
@@ -53,17 +87,18 @@ geometry_msgs::Twist calculateVelocity(Goal _goal,double pConstant){
  * @param tfBuffer pointer to tf buffer
  * @param _transformstamped pointer to the transform message
  */
-void updateTransform(geometry_msgs::TransformStamped *_transformstamped,int id) {
+void updateTransform(geometry_msgs::TransformStamped *_transformstamped,
+                     int id) {
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
-while(ros::ok()){
-  try {
-    *_transformstamped =
-        tfBuffer.lookupTransform("usb_cam", "marker_id"+std::to_string(id), ros::Time(0));
-  } catch (tf2::TransformException &ex) {
-    ROS_WARN("%s", ex.what());
-    ros::Duration(1.0).sleep();
+  while (ros::ok()) {
+    try {
+      *_transformstamped = tfBuffer.lookupTransform(
+          "usb_cam", "marker_id" + std::to_string(id), ros::Time(0));
+    } catch (tf2::TransformException &ex) {
+      ROS_WARN("%s", ex.what());
+      ros::Duration(1.0).sleep();
       continue;
+    }
   }
-}
 }
--- src/flipbot2_base/src/moveToGoal.cpp
+++ src/flipbot2_base/src/moveToGoal.cpp
@@ -8,32 +8,45 @@
 #include <dynamic_reconfigure/server.h>
 #include <tf2_ros/transform_listener.h>
 geometry_msgs::Twist cmd_msg;
-double linear_p = 0.5;
 geometry_msgs::TransformStamped transformStamped;
-std::vector<Goal> wayPoints = {Goal(x, 2)};
-
-void callback(flipbot2_base::flipbot2Config config, uint32_t level) {
-  ROS_INFO("Reconfigure Request:  %f ", config.proportional_control);
-  linear_p = config.proportional_control;
-}
+std::vector<Goal> wayPoints = {Goal(x, 0.5)};
+double linear_p = 0.5;
+double linear_tolerance = 0.5;
+void dyn_config();
+void callback(flipbot2_base::flipbot2Config config, uint32_t level);
 int main(int argc, char **argv) {
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
-  dynamic_reconfigure::Server<flipbot2_base::flipbot2Config> server;
-  dynamic_reconfigure::Server<flipbot2_base::flipbot2Config>::CallbackType f;
-  boost::thread thread_b(updateTransform, &transformStamped,4);
-  f = boost::bind(&callback, _1, _2);
-  server.setCallback(f);
+  ros::AsyncSpinner spinner(4);
+  spinner.start();
   ros::Publisher pub_cmdVel =
       n.advertise<geometry_msgs::Twist>("flipbot1/cmd_vel", 1000);
-  ros::Rate loop_rate(10);
-  cmd_msg.linear.x = 0.6;
+  dynamic_reconfigure::Server<flipbot2_base::flipbot2Config> server;
+  dynamic_reconfigure::Server<flipbot2_base::flipbot2Config>::CallbackType f;
+  f = boost::bind(&callback, _1, _2);
+  boost::thread thread_b(updateTransform, &transformStamped, 4);
+  server.setCallback(f);
+  VelocityController controller(&linear_p, &linear_tolerance,
+                                &transformStamped);
+  ros::Rate loop_rate(2);
   while (ros::ok()) {
-    calculateVelocity(wayPoints[1], linear_p);
     ROS_INFO("%lf", transformStamped.transform.translation.x);
-    pub_cmdVel.publish(cmd_msg);
-    ros::spinOnce();
+    controller.setGoal(wayPoints[0]);
+    while (!controller.inTolerance()) {
+      ROS_INFO("Publishing velocity");
+      cmd_msg = controller.calculateVelocity();
+      pub_cmdVel.publish(cmd_msg);
+      loop_rate.sleep();
+  }
     loop_rate.sleep();
   }
+  ros::waitForShutdown();
+
   return 0;
+}
+
+void callback(flipbot2_base::flipbot2Config config, uint32_t level) {
+  /* ROS_INFO("Reconfigure Request:  %f ", config.proportional_control); */
+  linear_p = config.proportional_control;
+  linear_tolerance = config.Linear_tolerance;
 }
