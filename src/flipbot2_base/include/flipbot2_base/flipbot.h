76d179cfd44c3a41bc9c728955243308a253369b
gokul <gokulakams@gmail.com>
2021-12-12 16:19:45 +0530

    added thread to update transform messages

--- src/flipbot2_base/include/flipbot2_base/flipbot.h
+++ src/flipbot2_base/include/flipbot2_base/flipbot.h
@@ -1,5 +1,10 @@
-#include "math.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "math.h" 
 #include <cmath>
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
 enum Axis { x, y };
 class Goal {
 public:
@@ -22,4 +27,43 @@ double euclidianDistance(double start, double end) {
   double distance = 0.0;
   distance = std::sqrt(pow((start - end), 2));
   return distance;
+}
+
+geometry_msgs::Twist calculateVelocity(Goal _goal,double pConstant){
+        geometry_msgs::Twist _twist;
+        if(_goal.axis == x ){
+                ROS_INFO("Moving in X direction");
+                double _linearVel = euclidianDistance(0,_goal.point);
+                _twist.linear.x = 0;
+        }
+        if(_goal.axis == y ){
+                ROS_INFO("Moving in Y direction");
+                double _linearVel = euclidianDistance(0,_goal.point);
+                _twist.linear.y = _linearVel * pConstant;
+        }
+
+        return _twist;
+}
+
+
+/**
+ * @brief updates the transform of the robot.Intentionally created to run as
+ * thread
+ *
+ * @param tfBuffer pointer to tf buffer
+ * @param _transformstamped pointer to the transform message
+ */
+void updateTransform(geometry_msgs::TransformStamped *_transformstamped,int id) {
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+while(ros::ok()){
+  try {
+    *_transformstamped =
+        tfBuffer.lookupTransform("usb_cam", "marker_id"+std::to_string(id), ros::Time(0));
+  } catch (tf2::TransformException &ex) {
+    ROS_WARN("%s", ex.what());
+    ros::Duration(1.0).sleep();
+      continue;
+  }
+}
 }
--- src/flipbot2_base/src/moveToGoal.cpp
+++ src/flipbot2_base/src/moveToGoal.cpp
@@ -1,44 +1,39 @@
- #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Twist.h"
 #include "flipbot2_base/flipbot.h"
-#include <dynamic_reconfigure/server.h>
 #include "flipbot2_base/flipbot2Config.h"
+#include "geometry_msgs/Twist.h"
+#include "ros/init.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <boost/core/ref.hpp>
+#include <dynamic_reconfigure/server.h>
+#include <tf2_ros/transform_listener.h>
 geometry_msgs::Twist cmd_msg;
 double linear_p = 0.5;
+geometry_msgs::TransformStamped transformStamped;
+std::vector<Goal> wayPoints = {Goal(x, 2)};
 
 void callback(flipbot2_base::flipbot2Config config, uint32_t level) {
-  ROS_INFO("Reconfigure Request:  %f ", 
-            config.proportional_control);
+  ROS_INFO("Reconfigure Request:  %f ", config.proportional_control);
   linear_p = config.proportional_control;
-}
-
-std::vector<Goal> wayPoints = {Goal(x,2)};
-geometry_msgs::Twist calculateVelocity(Goal _goal){
-        geometry_msgs::Twist _twist;
-        if(_goal.axis == x ){
-                ROS_INFO("Moving in X direction");
-                double _linearVel = euclidianDistance(0,_goal.point);
-                _twist.linear.x = 0;
-        }
-        return _twist;
 }
 int main(int argc, char **argv) {
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
   dynamic_reconfigure::Server<flipbot2_base::flipbot2Config> server;
   dynamic_reconfigure::Server<flipbot2_base::flipbot2Config>::CallbackType f;
-
+  boost::thread thread_b(updateTransform, &transformStamped,4);
   f = boost::bind(&callback, _1, _2);
   server.setCallback(f);
-  ros::Publisher pub_cmdVel= n.advertise<geometry_msgs::Twist>("flipbot1/cmd_vel", 1000);
+  ros::Publisher pub_cmdVel =
+      n.advertise<geometry_msgs::Twist>("flipbot1/cmd_vel", 1000);
   ros::Rate loop_rate(10);
-  cmd_msg.linear.x=0.6;
+  cmd_msg.linear.x = 0.6;
   while (ros::ok()) {
-        calculateVelocity(wayPoints[1]);
-        pub_cmdVel.publish(cmd_msg);
-        ros::spinOnce();
-        loop_rate.sleep();
+    calculateVelocity(wayPoints[1], linear_p);
+    ROS_INFO("%lf", transformStamped.transform.translation.x);
+    pub_cmdVel.publish(cmd_msg);
+    ros::spinOnce();
+    loop_rate.sleep();
   }
   return 0;
 }
