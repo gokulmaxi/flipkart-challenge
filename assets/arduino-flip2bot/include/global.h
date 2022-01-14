013f2756c17f5cff6ed4034d27974bfd22dfd60a
RoopanJKR <roopanyt@gmail.com>
2022-01-15 03:22:59 +0530

    Added package sorting using color sensor to ESP32

--- assets/arduino-flip2bot/include/global.h
+++ assets/arduino-flip2bot/include/global.h
@@ -3,46 +3,55 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "motorGlobal.h"
-#include "DFRobot_AS7341.h"
 #include <math.h>
 #include <Wire.h>
+#include <string.h>
+#include <Adafruit_TCS34725.h>
+#include <SPI.h>
+
+//Todo Change Device name, topic, destination topic according to bot number.
+char device_name[] = "ESP32-bot1";
+char device_topic[] = "flipkart/bot1";
+char destination_topic[] = "flipkart/color1";
+
+void callback(char *topic, byte *message, unsigned int length);
 
 int wifiled = 5;
 
+const int angularPwm = 180;
+const int linearPwm = 200;
+
 Servo servo;
 
-DFRobot_AS7341 as7341;
-// color data
+//Retriving Color from RGB data from TCS3475 RGB sensor
 
- int red_array[] = {9, 21, 23, 13, 15, 45, 152, 152};
+int red[] = {94, 69, 82};
 
- int green_array[] = {17, 20, 21, 80, 141, 33, 21, 29};
+int green[] = {28, 90, 94};
 
- int dark_blue[] = {8, 21, 30, 14, 4, 20, 32, 13};
+int dark_blue[] = {31, 83, 113};
 
- int yellow[] = {18, 10, 35, 60, 184, 251, 295, 207};
+int yellow[] = {74, 74, 64};
 
- int violet[] = {27, 88, 141, 62, 70, 70, 91, 101};
+int violet[] = {40, 77, 111};
 
- int brown[] = {16, 30, 59, 23, 40, 69, 100, 85};
+int brown[] = {65, 82, 85};
 
- int white[] = {49, 222, 416, 388, 478, 474, 535, 380};
+int white[] = {66, 96, 65};
 
- int pink[] = {62, 149, 232, 139, 148, 311, 521, 377};
+int pink[] = {97, 59, 89};
 
- int orange[] = {36, 16, 26, 31, 41, 170, 365, 276};
+int orange[] = {182, 36, 36};
 
-// networking
+// Networking Credentials
+
 WiFiClient espClient;
 PubSubClient client(espClient);
-char msg[50];
-int value = 0;
-const char *device_name = "ESP32-bot12"; //Change for each bot......(IMPORTANT)
 const char *ssid = "IA Lab";
 const char *password = "rosdeveloper";
 const char *mqtt_server = "192.168.0.172";
 
-//util functions
+//util compare function
 bool eq(int n, int *x, int *y, int tolerance)
 {
     const int absTolerance = std::abs(tolerance);
@@ -56,7 +65,7 @@ bool eq(int n, int *x, int *y, int tolerance)
     return true;
 }
 
-//set up ledc
+//setup Ledc
 void setupLedc()
 {
     ledcSetup(ledChannel, freq, resolution);
@@ -67,6 +76,7 @@ void setupLedc()
     ledcSetup(ledChannel5, freq, resolution);
     ledcSetup(ledChannel6, freq, resolution);
     ledcSetup(ledChannel7, freq, resolution);
+
     ledcAttachPin(motor4Pin1, ledChannel5); // Motor 4 D26
     ledcAttachPin(motor4Pin2, ledChannel4); //Motor 4 D27
 
@@ -93,34 +103,32 @@ void setupLedc()
 
     pinMode(wifiled, OUTPUT);
 }
-
+//Setup WiFi properties
 void setup_wifi()
 {
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(ssid);
-
     WiFi.begin(ssid, password);
-    // int attempts = 0;
+
     while (WiFi.status() != WL_CONNECTED)
     {
-        digitalWrite(wifiled , HIGH);
+        digitalWrite(wifiled, HIGH);
         delay(1000);
-        digitalWrite(wifiled,LOW);
+        digitalWrite(wifiled, LOW);
         delay(1000);
-        
+
         Serial.print(".");
     }
 
-    digitalWrite(wifiled , HIGH);
+    digitalWrite(wifiled, HIGH);
 
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(mqtt_server);
 }
-
-
+//Reconnect to WiFi
 void reconnect()
 {
     while (!client.connected())
@@ -130,8 +138,8 @@ void reconnect()
         if (client.connect(device_name))
         {
             Serial.println("connected");
-            //TODO - Use a variable to change topic and Device name
-            client.subscribe("flipkart/bot1"); //Change for each bot (IMPORTANT) -- Including Device name
+            client.subscribe(device_topic);
+            Serial.print(device_topic);
         }
         else
         {
@@ -140,5 +148,65 @@ void reconnect()
             Serial.println(" try again in 5 seconds");
             delay(5000);
         }
+    }
+}
+
+//Sorting packages with respect to colors
+int color()
+{   
+
+    Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+    float r, g, b;
+
+    tcs.getRGB(&r, &g, &b);
+
+    delay(50);
+
+    int currentRGB[] = {int(r), int(g), int(b)};
+
+    Serial.println(int(r));
+    Serial.println(int(g));
+    Serial.println(int(b));
+
+    if (eq(2, red, currentRGB, 5))
+    {
+        return 1; // PUNE
+    }
+    else if (eq(2, green, currentRGB, 5))
+    {
+        return 2; // AMEMDABAD
+    }
+    else if (eq(2, dark_blue, currentRGB, 5))
+    {
+        return 3; // JAIPUR
+    }
+    else if (eq(2, yellow, currentRGB, 5))
+    {
+        return 4; // CHENNAI
+    }
+    else if (eq(2, violet, currentRGB, 5))
+    {
+        return 5; // BENGALURU
+    }
+    else if (eq(2, brown, currentRGB, 5))
+    {
+        return 6; // HYDERABAD
+    }
+    else if (eq(2, white, currentRGB, 5))
+    {
+        return 7; // MUMBAI
+    }
+    else if (eq(2, pink, currentRGB, 5))
+    {
+        return 8; // DELHI
+    }
+    else if (eq(2, orange, currentRGB, 5))
+    {
+        return 9; //KOLKATA
+    }
+    else
+    {
+        return 0;
     }
 }
--- assets/arduino-flip2bot/include/motorGlobal.h
+++ assets/arduino-flip2bot/include/motorGlobal.h
@@ -1,17 +1,19 @@
-//Motor 4
-int motor4Pin1 = 26;
-int motor4Pin2 = 27;
+//Motor 1
+int motor1Pin1 = 18;
+int motor1Pin2 = 19;
 
 //Motor 2
 int motor2Pin1 = 32;
 int motor2Pin2 = 33;
 
-//Motor 1
-int motor1Pin1 = 18;
-int motor1Pin2 = 19;
 //Motor 3
 int motor3Pin1 = 12;
 int motor3Pin2 = 13;
+
+//Motor 4
+int motor4Pin1 = 26;
+int motor4Pin2 = 27;
+
 
 const int freq = 5000;
 
@@ -23,9 +25,11 @@ const int ledChannel4 = 12;
 const int ledChannel5 = 13;
 const int ledChannel6 = 14;
 const int ledChannel7 = 15;
-// pwm resolution
-const int resolution = 8;
 
+// PWM resolution
+
+const int resolution = 8;
+//Stop
 void allHalt()
 {
     ledcWrite(ledChannel, 0);
