e7c7446574eb30ebf9b1758b5bf7427d77409bd4
JKR <jkr@pop-os.localdomain>
2022-02-18 20:44:26 +0530

    Added Initiating functionality in Action Client

--- src/flipbot2_base/scripts/client.py
+++ src/flipbot2_base/scripts/client.py
@@ -54,27 +54,26 @@ class actionclient:
 
     def clientRoutine(self):
         while not rospy.is_shutdown():
-        control_bit=rosparam.get("bot_control")
-        if(control_bit == 1):
-            while not self.callbackCalled:
-                rospy.loginfo("waiting for color data")
-                if(self.result.inductIndex != 0):
-                    self.pub_colorReq.publish(self.result.inductIndex)
-                    rospy.sleep(3)
-                else:
-                    print("Initating")
-                    # TODO code for finding bots index
-                    try:
-                        trans = self.tfBuffer.lookup_transform(
-                            'world', 'marker_id'+str(self.bot_no), rospy.Time())
-                    except (self.tf2_ros.LookupException, self.tf2_ros.ConnectivityException, self.tf2_ros.ExtrapolationException):
-                        self.rate.sleep()
-                        continue
-                    
-                    if(abs(trans.transform.translation.x - 1.018) < 0.3):
-                        self.pub_colorReq.publish(1)
-                    elif(abs(trans.transform.translation.y - 1.773) < 0.3):
-                        self.pub_colorReq.publish(2)
+            control_bit = rospy.g("bot_control")
+            if(control_bit == 1):
+                while not self.callbackCalled:
+                    rospy.loginfo("waiting for color data")
+                    if(self.result.inductIndex != 0):
+                        self.pub_colorReq.publish(self.result.inductIndex)
+                        rospy.sleep(3)
+                    else:
+                        print("Initating")
+                        try:
+                            trans = self.tfBuffer.lookup_transform(
+                                'world', 'marker_id'+str(self.bot_no), rospy.Time())
+                        except (self.tf2_ros.LookupException, self.tf2_ros.ConnectivityException, self.tf2_ros.ExtrapolationException):
+                            self.rate.sleep()
+                            continue
+                        # Find Induct and publish
+                        if(abs(trans.transform.translation.x - 1.018) < 0.3):
+                            self.pub_colorReq.publish(1)
+                        elif(abs(trans.transform.translation.y - 1.773) < 0.3):
+                            self.pub_colorReq.publish(2)
 
     def servodir(self):
         if self.result.destIndex in right_one_index:
